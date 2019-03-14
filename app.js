ed5faa233b7e75537231c05ab328bca7ebc33956
Usuario <rflores9@ucol.mx>
2019-03-13 22:37:53 -0600

    0219 Prototipos

--- app.js
+++ app.js
@@ -30,3 +30,43 @@ function Person(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
 }
+
+//Prototipo es un objeto especial al que todos los objetos a puntan
+//con la palara recervada protoipo a algo puedo agregar una propiedad extraque no esta establecida en su constructor
+Person.prototype.greet= function(){
+  console.log('Hello, '+ this.firstName + ' ' + this.lastName);
+}
+
+var john = new Person ('John', 'Smith');
+//console.log(john(firstName));
+john.greet();
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
+
+//pass by value
+function change(b){
+  b = 2;
+
+}
+
+var a = 1;
+change(a);
+console.log(a);
+
+//pass by reference
+
+function changeObj(d){
+  d.prop1 = function() {}
+  d.prop2 = {};
+
+}
+
+var c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
